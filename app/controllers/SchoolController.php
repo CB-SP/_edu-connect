6c517d65aa38c2be2ec8e656080011a2efb3c23d
doctorscript <kelsondesousa99@gmail.com>
2026-03-02 15:59:06 +0100

    feature/update-fetch-delete-schools

--- app/controllers/SchoolController.php
+++ app/controllers/SchoolController.php
@@ -23,5 +23,65 @@
 
             return true;
         }
+
+        //edit shools
+        public function edit_school($name, $address, $contact_1, $contact_2, $logo, $id) {
+            if (empty($name) || empty($address) || empty($contact_1) || empty($id)) {
+                return false;
+            }
+
+            try {
+                if (!($this->school->edit_school($name, $address, $contact_1, $contact_2, $logo, $id))) {
+                    return false;
+                }
+            } catch (PDOException $e) {
+                error_log("ERRO_EDITAR_ESCOLA: ". $e->getMessage(). "\n". $e->getTraceAsString());
+                return false;
+            }
+
+            return true;
+        }
+
+        //search for every schools
+        public function fetch_schools() {
+            try {
+                return $this->school->fetch_schools();
+            } catch (PDOException $e) {
+                error_log("ERRO_BUSCAR_ESCOLAS: ". $e->getMessage(). "\n". $e->getTraceAsString());
+                return null;
+            }
+        }
+
+        //search for a unic school
+        public function fetch_school($id) {
+            if (empty($id)) {
+                return null;
+            }
+
+            try {
+                return $this->school->fetch_school($id);
+            } catch (PDOException $e) {
+                error_log("ERRO_BUSCAR_ESCOLA: ". $e->getMessage(). "\n". $e->getTraceAsString());
+                return null;
+            }
+        }
+
+        //delete schools
+        public function delete_school($id) {
+            if (empty($id)) {
+                return false;
+            }
+
+            try {
+                if (!($this->school->delete_school($id))) {
+                    return false;
+                }
+            } catch (PDOException $e) {
+                error_log("ERRO_DELETAR_ESCOLA: ". $e->getMessage(). "\n". $e->getTraceAsString());
+                return false;
+            }
+
+            return true;
+        }
     }
 ?>
