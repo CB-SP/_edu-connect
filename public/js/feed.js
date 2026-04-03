28e35508abd82da9c2b343af26d6996fe4915299
sérgioSp <sergiosapalalo16@gmail.com>
2026-04-03 23:02:19 +0100

    Built the publication cards & added some details in nav itens

--- public/js/feed.js
+++ public/js/feed.js
@@ -1,6 +1,48 @@
+//TOGGLE PUBLICATION MODAL 
 const btnNewPost = document.querySelector('.btn-new-publish')
+const closeIcon = document.querySelector('.close-icon-btn')
 const feedModal = document.querySelector('.feed-modal')
+const overlayFeed = document.querySelector('.overlay-feed')
 
 btnNewPost.addEventListener('click', () => {
+    overlayFeed.classList.add('active')
+    feedModal.classList.add('show')
+})
+
+closeIcon.addEventListener('click', () => {
+    feedModal.classList.remove('show')
+    overlayFeed.classList.remove('active')
+})
+
+overlayFeed.addEventListener('click', () => {
+    feedModal.classList.remove('show')
+    overlayFeed.classList.remove('active')
+})
+
+//DISPLAY COMMENT AREA
+const btnComments = document.querySelectorAll('.comment')
+
+btnComments.forEach(btnComment => {
+    btnComment.addEventListener('click', () => {
+        const commentBox = btnComment.parentElement.nextElementSibling
+        commentBox.classList.toggle('showComment')
+    })
+
+})
+
+
+const btnLikes = document.querySelectorAll('.thumbs-up')
+const btnFavorites = document.querySelectorAll('.favorite')
+
+btnLikes.forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.classList.toggle('click')
+    })
+})
+
+btnFavorites.forEach(btn=>{
+    btn.addEventListener('click', ()=>{
+       btn.classList.toggle('click') 
+    })
 
 })
