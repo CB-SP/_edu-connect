ae029b7aecd0c4800fc4c4bd69f27770a5f20a21
sérgioSp <sergiosapalalo16@gmail.com>
2026-04-13 21:11:20 +0100

    Finished the header of class page, modal & so on

---
+++ public/js/class.js
@@ -0,0 +1,19 @@
+const btnNewClass = document.querySelector('.btn-new-class')
+const classModal = document.querySelector('.class-modal')
+const overlayClass = document.querySelector('.overlay-class')
+const btnClose = document.querySelector('.close-icon-btn')
+
+btnNewClass.addEventListener('click', ()=>{
+    classModal.classList.add('showC')
+    overlayClass.classList.add('activeOverlayClass')
+})
+
+btnClose.addEventListener('click', ()=>{
+    classModal.classList.remove('showC')
+    overlayClass.classList.remove('activeOverlayClass')
+})
+
+overlayClass.addEventListener('click', ()=>{
+    classModal.classList.remove('showC')
+    overlayClass.classList.remove('activeOverlayClass')
+})
