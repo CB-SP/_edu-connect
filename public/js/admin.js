8bd83532704a26d160993066334a4bc5e64d28e7
sérgioSp <sergiosapalalo16@gmail.com>
2026-04-23 12:21:51 +0100

    currentClass tabs alternation done

--- public/js/admin.js
+++ public/js/admin.js
@@ -32,31 +32,21 @@ document.addEventListener('click', (e) => {
 const tabs = document.querySelectorAll('.tab')
 const contents = document.querySelectorAll('.tab-content')
 
-tabs.forEach(tab => {
-    tab.addEventListener('click', (e) => {
-        tabs.forEach(tab => tab.classList.remove('active'))
+if (tabs) {
+    tabs.forEach(tab => {
+        tab.addEventListener('click', (e) => {
+            tabs.forEach(tab => tab.classList.remove('active'))
 
-        contents.forEach(content => content.classList.remove('active'))
+            contents.forEach(content => content.classList.remove('active'))
 
-        tab.classList.add('active')
+            tab.classList.add('active')
 
-        const target = document.getElementById(tab.dataset.target)
-        target.classList.add('active')
+            const target = document.getElementById(tab.dataset.target)
+            target.classList.add('active')
+        })
     })
-})
+}
 
-/*ACTIVED LINK EFFECT*/
-
-const navItens = document.querySelectorAll('.nav-item')
-navItens.forEach(navItem => {
-
-    navItem.addEventListener('click', () => {
-
-        navItens.forEach(n => n.classList.remove('visited'))
-
-        navItem.classList.add('visited')
-    })
-})
 
 /*INPUT FILE SHOW NAME*/
 
@@ -113,32 +103,36 @@ const overlayModal = document.querySelector('.overlay-modal')
 
 
 /*ADD CLICKING ON THE BUTTON*/
-addBtns.forEach(addBtn => {
-    addBtn.addEventListener('click', (e) => {
-        e.stopPropagation()
-        addModais.forEach(addModal => {
-            addModal.classList.add('show')
-            document.body.classList.add('no-scroll')
-            addModal.classList.remove('no-scroll')
-            overlayModal.classList.add('active')
+if (addBtns) {
+    addBtns.forEach(addBtn => {
+        addBtn.addEventListener('click', (e) => {
+            e.stopPropagation()
+            addModais.forEach(addModal => {
+                addModal.classList.add('show')
+                document.body.classList.add('no-scroll')
+                addModal.classList.remove('no-scroll')
+                overlayModal.classList.add('active')
+            })
         })
-    })
 
-})
+    })
+}
 
 /*REMOVE CLICKING ON THE CLOSE ICON*/
-closeModalIcons.forEach(closeModalIcon => {
-    closeModalIcon.addEventListener('click', (e) => {
-        e.stopPropagation()
-        addModais.forEach(addModal => {
+if (closeModalIcons) {
+    closeModalIcons.forEach(closeModalIcon => {
+        closeModalIcon.addEventListener('click', (e) => {
+            e.stopPropagation()
+            addModais.forEach(addModal => {
 
-            addModal.classList.remove('show')
-            document.body.classList.remove('no-scroll')
-            overlayModal.classList.remove('active')
+                addModal.classList.remove('show')
+                document.body.classList.remove('no-scroll')
+                overlayModal.classList.remove('active')
 
+            })
         })
     })
-})
+}
 
 /*REMOVE CLICKING ON ENTIRE SCREEN*/
 document.addEventListener('click', () => {
@@ -161,59 +155,62 @@ addModais.forEach(m => {
 const editBtns = document.querySelectorAll('.edit-action')
 editModais = document.querySelectorAll('.edit-modal')
 
+if (editBtns) {
+    editBtns.forEach(editBtn => {
+        editBtn.addEventListener('click', async (e) => {
+            e.stopPropagation()
 
-editBtns.forEach(editBtn => {
-    editBtn.addEventListener('click', async (e) => {
-        e.stopPropagation()
+            const id = e.currentTarget.dataset.id
+            const type = e.currentTarget.dataset.type
 
-        const id = e.currentTarget.dataset.id
-        const type = e.currentTarget.dataset.type
+            if (type == "user") {
+                const response = await fetch(`http://localhost/_edu-connect/admin/fetch_user/${id}`)
+                const user = await response.json()
 
-        if (type == "user") {
-            const response = await fetch(`http://localhost/_edu-connect/admin/fetch_user/${id}`)
-            const user = await response.json()
+                document.querySelector('#userId').value = id
+                document.querySelector('#userName').value = user.nome
+                document.querySelector('#userPrimaryContact').value = user.contacto_1
+                document.querySelector('#userSecundaryContact').value = user.contacto_2
+                document.querySelector('#nifUser').value = user.nif
+                document.querySelector('#emailUser').value = user.email
+                document.querySelector('#current_photo').value = user.foto || ''
+            } else {
+                const response = await fetch(`http://localhost/_edu-connect/admin/fetch_school/${id}`)
+                const school = await response.json()
 
-            document.querySelector('#userId').value = id
-            document.querySelector('#userName').value = user.nome
-            document.querySelector('#userPrimaryContact').value = user.contacto_1
-            document.querySelector('#userSecundaryContact').value = user.contacto_2
-            document.querySelector('#nifUser').value = user.nif
-            document.querySelector('#emailUser').value = user.email
-            document.querySelector('#current_photo').value = user.foto || ''
-        } else {
-            const response = await fetch(`http://localhost/_edu-connect/admin/fetch_school/${id}`)
-            const school = await response.json()
+                document.querySelector('#schoolName').value = school.nome
+                document.querySelector('#schoolAddress').value = school.endereco
+                document.querySelector('#primaryContact').value = school.contacto_1
+                document.querySelector('#secundaryContact').value = school.contacto_2
+                document.querySelector('#schoolId').value = id
+            }
 
-            document.querySelector('#schoolName').value = school.nome
-            document.querySelector('#schoolAddress').value = school.endereco
-            document.querySelector('#primaryContact').value = school.contacto_1
-            document.querySelector('#secundaryContact').value = school.contacto_2
-            document.querySelector('#schoolId').value = id
-        }
+            editModais.forEach(editModal => {
+                editModal.classList.add('show')
+                document.body.classList.add('no-scroll')
+                overlayModal.classList.add('active')
 
-        editModais.forEach(editModal => {
-            editModal.classList.add('show')
-            document.body.classList.add('no-scroll')
-            overlayModal.classList.add('active')
-
-            dropActions.forEach(dropAction => {
-                dropAction.classList.remove('drop')
+                dropActions.forEach(dropAction => {
+                    dropAction.classList.remove('drop')
+                })
             })
         })
     })
-})
+}
 
-closeModalIcons.forEach(closeModalIcon => {
-    closeModalIcon.addEventListener('click', (e) => {
-        e.stopPropagation()
+if (closeModalIcons) {
+    closeModalIcons.forEach(closeModalIcon => {
+        closeModalIcon.addEventListener('click', (e) => {
+            e.stopPropagation()
 
-        editModais.forEach(editModal => {
-            editModal.classList.remove('show')
-            document.body.classList.remove('no-scroll')
-            overlayModal.classList.remove('active')
+            editModais.forEach(editModal => {
+                editModal.classList.remove('show')
+                document.body.classList.remove('no-scroll')
+                overlayModal.classList.remove('active')
+            })
         })
     })
-})
+}
 
 document.body.addEventListener('click', () => {
     editModais.forEach(editModal => {
@@ -235,13 +232,17 @@ editModais.forEach(editModal => {
 const editAdminBtn = document.querySelector('#btn-edit-info-admin')
 const editAdminModal = document.querySelector('#edit-admin-modal')
 
-editAdminBtn.addEventListener('click', (e) => {
-    e.stopPropagation()
-    editAdminModal.classList.add('show')
-    document.body.classList.add('no-scroll')
-    overlayModal.classList.add('active')
+if (editAdminBtn) {
+    if (editAdminModal) {
+        editAdminBtn.addEventListener('click', (e) => {
+            e.stopPropagation()
+            editAdminModal.classList.add('show')
+            document.body.classList.add('no-scroll')
+            overlayModal.classList.add('active')
 
-    dropActions.forEach(dropAction => {
-        dropAction.classList.remove('drop')
-    })
-})
+            dropActions.forEach(dropAction => {
+                dropAction.classList.remove('drop')
+            })
+        })
+    }
+}
