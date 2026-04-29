4fa89adf74dd691ed8bd3942859f92e9886c0e6b
sérgioSp <sergiosapalalo16@gmail.com>
2026-04-29 23:25:05 +0100

    modal  of students management done

--- public/js/currentClass.js
+++ public/js/currentClass.js
@@ -22,3 +22,32 @@ contentClass.forEach(content => {
 
     })
 })
+
+//MODAL MANAGE STUDENTS*/
+const btn = document.querySelector('#btn-show-modal')
+const iconClose = document.querySelector('#close-menu-m')
+const btnCloseModal = document.querySelector('.btn-close-student-modal')
+const modal = document.querySelector('.modal-students-list')
+const overlayCurrentClas = document.querySelector('.overlay-current-class')
+
+btn.addEventListener('click', () => {
+    modal.classList.add('show')
+    overlayCurrentClas.classList.add('activeOver')
+})
+
+iconClose.addEventListener('click', () => {
+    modal.classList.remove('show')
+    overlayCurrentClas.classList.remove('activeOver')
+})
+
+overlayCurrentClas.addEventListener('click', ()=> {
+    modal.classList.remove('show')
+    overlayCurrentClas.classList.remove('activeOver')
+})
+
+btnCloseModal.addEventListener('click', () => {
+    modal.classList.remove('show')
+    overlayCurrentClas.classList.remove('activeOver')
+})
+
+console.log(iconClose)
