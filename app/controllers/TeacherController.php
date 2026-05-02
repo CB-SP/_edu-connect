31165b424d879c124adf6ba6ec9cfbc704a473db
sérgioSp <sergiosapalalo16@gmail.com>
2026-05-02 18:35:29 +0100

    other html studeent management added

--- app/controllers/TeacherController.php
+++ app/controllers/TeacherController.php
@@ -1,16 +1,25 @@
 <?php
-    class TeacherController extends UserController {
-        public function classes() {
-            $this->isLoged();
-            Utils::private_route('professor');
-            $this->show_page("classes");
-        }
-
-        public function class(int $id) {
-            $this->isLoged();
-            Utils::private_route('professor');
-            $_SESSION['class_id'] = $id;
-            $this->show_page("class");
-        }
+class TeacherController extends UserController
+{
+    public function classes()
+    {
+        $this->isLoged();
+        Utils::private_route('professor');
+        $this->show_page("classes");
     }
-?>
+
+    public function painel()
+    {
+        $this->isLoged();
+        Utils::private_route('professor');
+        $this->show_page("painel");
+    }
+
+    public function class(int $id)
+    {
+        $this->isLoged();
+        Utils::private_route('professor');
+        $_SESSION['class_id'] = $id;
+        $this->show_page("class");
+    }
+}
