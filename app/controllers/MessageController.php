a59ccb67f7855fac6e243d43d9e838d53a78dcd6
doctorscript08 <kelsondesousa99@gmail.com>
2026-05-21 21:31:07 +0100

    feature/unread-messages-added

--- app/controllers/MessageController.php
+++ app/controllers/MessageController.php
@@ -47,5 +47,37 @@
                 exit;
             }
         }
+
+        //get unread messages
+        public function get_unread_messages(int $id) {
+            if (empty($id)) {
+                return null;
+            }
+
+            try {
+                return $this->message->get_unread_messages($id);
+            } catch (PDOException $e) {
+                error_log("ERRO_BUSCAR_MENSAGENS_NAO_LIDAS: ". $e->getMessage(). "\n". $e->getTraceAsString());
+                return null;
+            }
+        }
+
+        //view messages
+        public function view_message(int $message, int $user, int $class, string $role) {
+            if (empty($message) || (empty($user)) || empty($role) || empty($class)) {
+                $this->redirect("user/index/error");
+            }
+
+            try {
+                if (!$this->message->view_message($message, $user)) {
+                    $this->redirect("user/index/error");
+                }
+            } catch (PDOException $e) {
+                error_log("ERRO_VISUALIZAR_MENSAGENS: ". $e->getMessage(). "\n". $e->getTraceAsString());
+                $this->redirect("user/index/error");
+            }
+
+            $this->redirect($role === "professor" ? "teacher/class/$class" : "student/class/$class");
+        }
     }
 ?>
